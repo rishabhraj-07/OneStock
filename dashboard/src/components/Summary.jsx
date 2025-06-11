fa8b7d41d40165513c030c813502efd0a6e77bee
Rishabh Raj <142199442+rishabhraj-07@users.noreply.github.com>
2025-06-11 13:35:18 +0530

    Update Summary.jsx

--- dashboard/src/components/Summary.jsx
+++ dashboard/src/components/Summary.jsx
@@ -8,7 +8,7 @@ function Summary() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/auth/verify", { withCredentials: true })
+      .get("https://onestock-backend.onrender.com/auth/verify", { withCredentials: true })
       .then((res) => {
         if (res.data.status) {
           setUsername(res.data.user); //  Set the username
